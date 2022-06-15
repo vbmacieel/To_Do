a5e16e5b7d4b74b400e986fe59c6d589b421c077
Victor Bastos Maciel <victor.bastos@flieger.software>
2022-06-15 19:09:04 -0300

    Creating the last layout add binding for it

--- app/src/main/java/com/example/todoapp/ui/dialog/CreateToDoDialog.java
+++ app/src/main/java/com/example/todoapp/ui/dialog/CreateToDoDialog.java
@@ -10,17 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import com.example.todoapp.databinding.CreateTodoDialogBinding;
+
 public class CreateToDoDialog extends DialogFragment {
+    private CreateTodoDialogBinding mBinding;
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        mBinding = CreateTodoDialogBinding.inflate(getLayoutInflater());
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setMessage("Tentativa de dialog");
+        builder.setView(mBinding.getRoot());
         builder.setPositiveButton("CERTO", (dialogInterface, i) ->
                 Toast.makeText(getContext(), "Clicado", Toast.LENGTH_SHORT).show());
-        builder.setNegativeButton("ERRADO", (dialogInterface, i) ->
-                Toast.makeText(getContext(), "Clicado", Toast.LENGTH_SHORT).show());
+        builder.setNegativeButton("ERRADO", (dialogInterface, i) -> dismiss());
         return builder.create();
     }
 }
